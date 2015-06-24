ed6e02ce4c2f38de52ba302bcabc3ca6ea92b2f0
briankasingli <brian.ksaing.li@gmail.com>
2015-06-24 02:41:15 +0000

    completed search on git hub

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,31 @@
+class SearchController < ApplicationController
+
+  def index
+    @results = Movie.all
+
+    if params[:title].present?
+      @results = @results.where(["title LIKE ?", "%#{params[:title]}%"])
+    end
+
+    if params[:director].present?
+      @results = @results.where(["director LIKE ?", "%#{params[:director]}%"])
+    end
+
+    #duration
+    case params[:duration]
+    when "Under 90"
+      @results = @results.where('runtime_in_minutes < ?', 90)
+    when "Between 90 and 120" 
+      @results =  @results.where("runtime_in_minutes >= ? AND runtime_in_minutes <= ?", 90, 120)
+    when "Over 120" 
+      @results = @results.where("runtime_in_minutes > ?", 120) 
+    end
+
+    @results = @results.page(params[:page]).per(1)
+
+  end
+
+end
+
+
+#Movie.where("title LIKE  ? ", "%%").where("director LIKE ? ", "%%").where("runtime_in_minutes  ? ", "90")
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ RottenMangoes::Application.routes.draw do
     resources :users
   end
 
+  get "search(/:termen)" => "search#index", :as => :search
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
